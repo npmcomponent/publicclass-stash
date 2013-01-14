4a6aaf90ec098f60ba545bf423eebbb09448c7be
Robert Sköld <robert@publicclass.se>
2013-01-14 22:51:17 +0100

    Added some tests for #has() and #length

--- test.js
+++ test.js
@@ -16,14 +16,18 @@ assert.strictEqual(stash.reverse[0],'a')
 assert.strictEqual(stash.reverse[1],'b')
 assert.strictEqual(stash.reverse[2],'2')
 
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,3)
 assert.strictEqual(stash.get('b'),b)
 assert.strictEqual(stash.get('a'),123)
+assert.strictEqual(stash.has('a'),true)
+assert.strictEqual(stash.has('x'),false)
 assert.strictEqual(stash.get('x'),undefined)
 assert(Array.isArray(stash.get(2)))
 
 stash.del(2)
 
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,2)
 assert.strictEqual(stash.lookup['a'],0)
 assert.strictEqual(stash.lookup['b'],1)
@@ -32,12 +36,14 @@ assert.strictEqual(stash.reverse[1],'b')
 
 stash.del('a')
 
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,1)
 assert.strictEqual(stash.lookup['b'],0)
 assert.strictEqual(stash.reverse[0],'b')
 
 stash.set('d',d)
 
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,2)
 assert.strictEqual(stash.values[0],b) // should have replaced 'a'
 assert.strictEqual(stash.values[1],d)
@@ -48,6 +54,7 @@ assert.strictEqual(Object.keys(stash.reverse).length, 2)
 stash.set('e',456)
 stash.del('b')
 
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,2)
 assert.strictEqual(stash.values[0],456) // replaced 'b'
 assert.strictEqual(stash.values[1],d)
@@ -67,6 +74,7 @@ stash.empty()
 assert.strictEqual(stash.values,vals) // make sure the values wasn't recreated
 assert.strictEqual(stash.lookup,look) // make sure the lookup wasn't recreated
 assert.strictEqual(stash.reverse,rev) // make sure the reverse wasn't recreated
+assert.strictEqual(stash.values.length,stash.length)
 assert.strictEqual(stash.values.length,0) // and it's empty
 assert.strictEqual(Object.keys(stash.lookup).length,0)
 assert.strictEqual(Object.keys(stash.reverse).length,0)
